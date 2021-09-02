d206cf6068e92bde1b915c19009ab0fb3f1c5975
CXRedix <1135058679@qq.com>
2021-09-03 01:07:00 +0800

    removed cycles from Timer, added basic 2x2 Matrix

--- Src/Timer/Timer.cs
+++ Src/Timer/Timer.cs
@@ -5,84 +5,56 @@ namespace CXUtils.Common
     /// <summary>
     ///     A timer that ticks by manual control
     /// </summary>
-    public class Timer : ICloneable, IFormattable
+    public class Timer : IFormattable
     {
-        public Timer( float maxSpan, bool cycleReset = true )
-        {
-            MaxSpan = maxSpan;
-            CycleReset = cycleReset;
-            FirstCycleCompleted = false;
-        }
-        
+        public Timer( float maxSpan ) => MaxSpan = maxSpan;
+
         public Timer( Timer other )
         {
             MaxSpan = other.MaxSpan;
             CurrentSpan = other.CurrentSpan;
-            CycleReset = other.CycleReset;
-            FirstCycleCompleted = other.FirstCycleCompleted;
         }
 
         public float MaxSpan { get; }
 
         public float CurrentSpan { get; private set; }
-        public bool CycleReset { get; }
 
-        public bool FirstCycleCompleted { get; private set; }
-
-        /// <summary>
-        ///     Deep clones the timer
-        /// </summary>
-        public object Clone() => new Timer( this );
-
-        public string ToString( string format, IFormatProvider formatProvider ) =>
-            "Current Span: " + CurrentSpan.ToString( format, formatProvider ) + ", Max Span: " + MaxSpan.ToString( format, formatProvider );
+        bool _timerEndedCheck;
 
         /// <summary>
         ///     Ticks the timer using the <see cref="delta" />
         /// </summary>
         public bool Tick( float delta )
         {
-            //if not gonna cycle reset and the first cycle is already completed
-            if ( !CycleReset && FirstCycleCompleted )
-                return false;
-
+            if ( _timerEndedCheck ) return false;
+            
             CurrentSpan += delta;
 
             //if current Timer is not over max timer
-            if ( !( CurrentSpan >= MaxSpan ) )
-                return false;
+            if ( !( CurrentSpan >= MaxSpan ) ) return false;
 
-            DoCycleCompleted();
+            _timerEndedCheck = true;
+            OnTriggered?.Invoke();
 
             return true;
         }
 
+        public void SetSpan( float span ) => CurrentSpan = span;
+
         /// <summary>
         ///     reset's the <see cref="CurrentSpan" /> back to initial value, but doesn't count for first cycle
         /// </summary>
-        void Reset() => CurrentSpan = 0;
-
-        /// <summary>
-        ///     set's the <see cref="CurrentSpan" /> back to initial value and resets the <see cref="FirstCycleCompleted" />
-        /// </summary>
-        void FullReset()
+        void Reset()
         {
-            CurrentSpan = 0;
-            FirstCycleCompleted = false;
+            CurrentSpan = 0f;
+            _timerEndedCheck = false;
         }
 
-        void DoCycleCompleted()
-        {
-            CurrentSpan = 0;
-
-            OnCycleComplete?.Invoke();
-
-            if ( !FirstCycleCompleted )
-                FirstCycleCompleted = true;
-        }
-
-        public event Action OnCycleComplete;
+        public event Action OnTriggered;
         
         public override string ToString() => "Current Span: " + CurrentSpan + ", Max Span: " + MaxSpan;
+
+        public string ToString( string format, IFormatProvider formatProvider ) =>
+            "Current Span: " + CurrentSpan.ToString( format, formatProvider ) + ", Max Span: " + MaxSpan.ToString( format, formatProvider );
     }
 }
---
+++ Src/Types/Matrix/Float2x2.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using CXUtils.Types.Utils;
+
+namespace CXUtils.Types.Matrix
+{
+    /// <summary>
+    ///     Represents a 2x2 floating point matrix
+    /// </summary>
+    [StructLayout( LayoutKind.Explicit )]
+    public readonly struct Float2X2 : IEquatable<Float2X2>
+    {
+        /// <summary>
+        ///     Left Column
+        /// </summary>
+        [FieldOffset( 0 )] public readonly Float2 iHat;
+        /// <summary>
+        ///     Right Column
+        /// </summary>
+        [FieldOffset( 8 )] public readonly Float2 jHat;
+
+        public Float2X2( Float4 value ) => ( iHat, jHat ) = ( value.XY(), value.ZW() );
+        public Float2X2( float leftUp, float leftDown, float rightUp, float rightDown ) =>
+            ( iHat, jHat ) = ( new Float2( leftUp, leftDown ), new Float2( rightUp, rightDown ) );
+        public Float2X2( Float2 iHat, Float2 jHat ) => ( this.iHat, this.jHat ) = ( iHat, jHat );
+
+        public Float2X2 Transposed  => new Float2X2( new Float2( iHat.x, jHat.x ), new Float2( iHat.y, jHat.y ) );
+        public float    Determinant => iHat.x * jHat.y - jHat.x * iHat.y;
+        public bool     IsSingular  => Determinant == 0f;
+
+        public static Float2X2 Zero     => new Float2X2( 0f, 0f, 0f, 0f );
+        public static Float2X2 Identity => new Float2X2( new Float2( 1f ), new Float2( 0f, 1f ) );
+
+        #region Operators
+
+        public static Float2X2 operator +( Float2X2 a, Float2X2 b ) => new Float2X2( a.iHat + b.iHat, a.jHat + b.jHat );
+        public static Float2X2 operator -( Float2X2 a, Float2X2 b ) => new Float2X2( a.iHat - b.iHat, a.jHat - b.jHat );
+
+        public static Float2X2 operator *( Float2X2 matrix, float scalar ) => new Float2X2( matrix.iHat * scalar, matrix.jHat * scalar );
+        public static Float2X2 operator *( Float2X2 a, Float2X2 b ) => new Float2X2(
+            new Float2( a.iHat.x * b.iHat.x + a.jHat.x * b.iHat.y, a.iHat.x * b.jHat.x + a.jHat.x * b.jHat.y ),
+            new Float2( a.iHat.y * b.iHat.x + a.jHat.y * b.iHat.y, a.iHat.y * b.jHat.x + a.jHat.y * b.jHat.y )
+        );
+
+        public static Float2X2 operator /( Float2X2 matrix, float scalar ) => new Float2X2( matrix.iHat / scalar, matrix.jHat / scalar );
+        public static Float2X2 operator /( Float2X2 a, Float2X2 b ) => a * b.GetInverse();
+
+        public static bool operator ==( Float2X2 left, Float2X2 right ) => left.iHat == right.iHat && left.jHat == right.jHat;
+
+        public static bool operator !=( Float2X2 left, Float2X2 right ) => left.iHat != right.iHat || left.jHat != right.jHat;
+
+        public static Float2X2 operator -( Float2X2 matrix ) => new Float2X2( -matrix.iHat, -matrix.jHat );
+
+        #endregion
+
+        #region Methods
+
+        public bool TryGetInverse( out Float2X2 inverse )
+        {
+            if ( Determinant == 0f )
+            {
+                inverse = default;
+                return false;
+            }
+
+            inverse = GetInverse();
+            return true;
+        }
+
+        public Float2X2 GetInverse()
+        {
+            Debug.Assert( IsSingular, "Cannot get the inverse of a singular matrix!" );
+            float scalar = 1f / Determinant;
+            return new Float2X2( new Float2( jHat.y * scalar, -iHat.y * scalar ), new Float2( -jHat.x * scalar, iHat.x * scalar ) );
+        }
+
+        public bool Equals( Float2X2 other ) => iHat.Equals( other.iHat ) && jHat.Equals( other.jHat );
+        public override bool Equals( object obj ) => obj is Float2X2 other && Equals( other );
+        public override int GetHashCode() => HashCode.Combine( iHat, jHat );
+
+        #endregion
+
+        #region Static Methods
+
+        public static Float2X2 BuildDiagonal( Float2 value ) => new Float2X2( new Float2( value.x ), new Float2( 0f, value.y ) );
+        public static Float2X2 BuildScalar( float value ) => new Float2X2( new Float2( value ), new Float2( 0f, value ) );
+
+        #endregion
+    }
+}
