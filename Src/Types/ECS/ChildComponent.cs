88a27299789e3399b08ec527c30e8296a1675390
CXRedix <1135058679@qq.com>
2021-09-12 16:06:47 +0800

    Moved files and added ECS

---
+++ Src/Types/ECS/ChildComponent.cs
@@ -0,0 +1,12 @@
+﻿namespace CXUtils.Types
+{
+    /// <summary>
+    ///     represents a child component of the specified type <typeparamref name="T" />
+    /// </summary>
+    public abstract class ChildComponent<T>
+    {
+        public readonly T root;
+
+        protected ChildComponent( T root ) => this.root = root;
+    }
+}
